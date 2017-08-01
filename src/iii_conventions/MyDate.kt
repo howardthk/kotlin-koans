efb73714e17fec73c3a1981a8300a05919cd65b6
Howard Tam <howardthk@gmail.com>
2017-08-01 12:34:51 +0800

    iii_conventions answer

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,13 +1,49 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.sql.Time
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int =
+            when {
+                year != other.year -> year - other.year
+                month != other.month -> month - other.month
+                else -> dayOfMonth - other.dayOfMonth
+            }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(start = this, endInclusive = other)
 
 enum class TimeInterval {
+
     DAY,
     WEEK,
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate) : Iterable<MyDate> {
+
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+
+    operator fun contains(d: MyDate): Boolean = start <= d && d <= endInclusive
+}
+
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+
+    var current: MyDate = dateRange.start
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val result = current;
+        current = current.nextDay()
+        return result
+    }
+
+}
+
+operator fun MyDate.plus(timeInterval: TimeInterval) = this.addTimeIntervals(timeInterval = timeInterval, number = 1)
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(timeInterval = this, number = number)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval) = this.addTimeIntervals(timeInterval = repeatedTimeInterval.timeInterval, number = repeatedTimeInterval.number)
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,17 +3,25 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+class Invokable(private var invocation: Int = 0) {
+
+    operator fun invoke(): Invokable {
+        invocation++
+        return this
+    }
+
+    fun getNumberOfInvocations(): Int = invocation
+}
 
 fun todoTask31(): Nothing = TODO(
-    """
+        """
         Task 31.
         Change the class 'Invokable' to count the number of invocations:
         for 'invokable()()()()' it should be 4.
     """,
-    references = { invokable: Invokable -> })
+        references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+//    todoTask31()
+    return invokable()()()().getNumberOfInvocations()
 }
